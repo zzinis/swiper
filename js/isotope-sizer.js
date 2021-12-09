2136325909008afe9e266a550924d0748a665b97
zzinis <lizzyin@naver.com>
2021-12-09 18:33:17 +0900

    sld4 isotope-sizer js 수정

--- js/isotope-sizer.js
+++ js/isotope-sizer.js
@@ -6,11 +6,35 @@
 //         columWidth: ".item-sizer"
 //     }
 // })
-$("section").isotope({
-    itemSelector: '.item',
-    percentPosition: true,
-    masonry: {
-      columnWidth: '.item-sizer'
-    }
-  });
-  
+
+// $('.grid').isotope({
+//     itemSelector: '.item',
+//     percentPosition: true,
+//     masonry: {
+//       columnWidth: '.item-sizer'
+//     }
+//   });
+
+//   var elem = document.querySelector('.grid');
+
+
+// // element argument can be a selector string
+// //   for an individual element
+// var iso = new Isotope( '.grid', {
+//   // options
+//   itemSelector: '.item',
+//   percentPosition: true,
+//     masonry: {
+//       columnWidth: '.item-sizer'
+//     }
+// });
+
+$('section').isotope({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.item',
+  percentPosition: true,
+  masonry: {
+    // set to the element
+    columnWidth: '.item-sizer'
+  }
+})
